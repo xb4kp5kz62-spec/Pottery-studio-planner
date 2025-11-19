fe8cfebd7f66e725a06e0208a6365e38a65b62a3
xb4kp5kz62-spec <xb4kp5kz62@privaterelay.appleid.com>
2025-11-19 12:53:54 -0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,3 +1,4 @@
+```typescript
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -8,3 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+```
